2ac5d704ed3d6282f94d35d51292b506571dee97
Adithya <32417423+adithyakv100@users.noreply.github.com>
2020-03-07 07:41:05 +0530

    Update globalRouting.js

--- src/globalRouting.js
+++ src/globalRouting.js
@@ -4,6 +4,10 @@ const router = Router();
 
 const userRouter = require("./apps/User/user.routes");
 
+router.get("/", function(req, res) {
+  res.send("Welcome to Market-Boats. It's running!");
+});
+
 router.use("/auth", userRouter);
 
 module.exports = {
