dfe74729762e6523a0b98e88c75f74d1c3a1fcbe
Adithya <32417423+adithyakv100@users.noreply.github.com>
2020-03-07 09:16:38 +0530

    Changed to running the node server on: 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { app } = require("./loaders");
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000!");
+app.listen(80, () => {
+  console.log("Server started on port 80!");
 });
 
 module.exports = app;
